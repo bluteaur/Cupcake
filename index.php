0f09934db9a164eb89b2663da71c9e7106722017
bluteaur <bluteaur@uwindsor.ca>
2015-07-12 00:42:21 -0400

    Layout I described in README.md

--- index.php
+++ index.php
@@ -1,1 +1,45 @@
-i
+<?php
+  /*start session here */
+
+  /* code that'll check $_POST['signup'] 
+                  add information to database (username + password)
+                  check if everything is valid
+                        if not then print error message
+                  set session['signup'] = true
+  */
+
+?>
+
+<?php
+  /* code that'll check $_POST['login'] === true || $_SESSION['signup'] === true
+                  set $_SESSION['login'] = true
+                  set $_SESSION['username'] = $_POST['username']
+  */
+
+?>
+
+<?php
+  /*code that'll check $_SESSION['login'] === true 
+                  if true, redirect to main.php and exit
+  */
+?>
+
+<!doctype html>
+<html>
+  <head>
+    <title>Cupcake Messaging Login</title>
+  </head>
+  <body>
+    <?php
+      /*code that'll display the login/signup stuff
+              FORM:
+                    action = index.php
+                    $_POST['username']
+                    $_POST['password']
+    FROM BUTTONS    $_POST['login']   or  $_POST['signup']
+      */
+    ?>
+  </body>
+</html>
+
+<!-- java script to check validations goes here -->
