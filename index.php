70c621b00d627a9e5bb1c6c55ec377f6e604a2ac
bluteaur <bluteaur@uwindsor.ca>
2015-07-14 22:36:03 -0400

    Put in some example code

--- index.php
+++ index.php
@@ -1,20 +1,45 @@
 <?php
   /*start session here */
-
-  /* code that'll check $_POST['signup'] 
+  session_start();
+  
+  /* code that'll check isset($_POST['signup'])
                   add information to database (username + password)
                   check if everything is valid
                         if not then print error message 
                         else set session['signup'] = true
   */
+  $UserNameMatch = true;
+  $PasswordMatch = true;
+  if(isset($_POST['signup'])){
+    //check data and set UserNameMatch and PasswordMatch
+    //put info in database
+    //check database errors
+    $_SESSION['signup'] = true;
+  }
 
 ?>
 
 <?php
-  /* code that'll check $_POST['login'] === true || $_SESSION['signup'] === true
+  /* code that'll check isset($_POST['login']) || isset($_SESSION['signup'])
                   set $_SESSION['login'] = true
                   set $_SESSION['username'] = $_POST['username']
   */
+  if($_SESSION['signup'] === true || isset($_POST['login'])){
+    //get username + password from database
+    //match username + password to an account and set variables below
+    $UserNameMatch = false;
+    $PasswordMatch = false;
+    if($match){
+      $_SESSION['login'] = true;
+        //look in database and see if user is an admin
+      $_SESSION['admin'] = false; //admin creation
+        //here we will gather any additionnal information needed from database
+      $_SESSION['username'] = $_POST['username'];
+    } else {
+      $_SESSION['login'] = false;
+      $_SESSION['admin'] = false;
+    }
+  }
 
 ?>
 
@@ -22,6 +47,12 @@
   /*code that'll check $_SESSION['login'] === true 
                   if true, redirect to main.php and exit
   */
+  
+  if($_SESSION['login'] === true){
+    header('Location: main.php');
+    exit;
+  }
+  
 ?>
 
 <!doctype html>
@@ -38,6 +69,25 @@
                     $_POST['password']
     FROM BUTTONS    $_POST['login']   or  $_POST['signup']
       */
+  echo '<div>';
+  echo '<form action="index.php" method="post">';
+if(!$UserNameMatch)
+  echo '<span style = "font-color:red">';
+  echo 'Username: ';
+if(!$UserNameMatch)
+  echo '*<\span>';
+  echo '<input type="text" name="username"> <br>';
+if(!$PasswordMatch)
+  echo '<span style = "font-color:red">';
+  echo 'Password: ';
+if(!$PasswordMatch)
+  echo '*<\span>';
+  echo '<input type="password" name="password" autocomplete="off"> <br>';
+  echo '<input type="submit" name="signup" value="Signup">';
+  echo '<input type="submit" name="login" value="Login">';
+  echo '</form>';
+  echo '</div>';
+
     ?>
   </body>
 </html>
