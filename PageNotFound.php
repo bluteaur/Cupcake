e560711d1d2d59ed26a5659dbdda05187a3d6d32
bluteaur <bluteaur@uwindsor.ca>
2015-07-15 23:52:03 -0400

    Create PageNotFound.php

---
+++ PageNotFound.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Cupcake Messaging Login</title>
+    <link rel="stylesheet" type="text/css" href="cupcake.css">
+    <meta name="Cupcake Messaging" content="Your messaging site!">
+    <meta name="Cupcake Messaging" content="We don't sell your information. You have control.">
+  </head>
+  <body>
+    <div id="LoginForm">I think you don't belong here so.. Let me MEOW at you :3 
+    <a href="index.php">Click Here To Escape!</a>
+    <?php
+      for($i = 0; $i < 20; $i++)
+        echo '<span id="invalid">MENOW</span>';
+    ?>
+    </div>
+  </body>
+</html>
