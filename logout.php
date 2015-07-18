f63db740f30e7011b7b4b87746b2758ec08c68b8
bluteaur <bluteaur@uwindsor.ca>
2015-07-18 12:13:47 -0400

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,10 @@
 <?php
   session_start();
+  if($_SESSION['login'] == false || !isset($_SESSION['login']) || $_SESSION['bot']){
+    header('Location: PageNotFound.php');
+    exit;
+  }
+  $_SESSION['mobile'] = null;
   $_SESSION['login'] = false;
   $_SESSION['username'] = null;
   $_SESSION['admin'] = false;
