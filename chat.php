83a5a8fd55ffb0c93b61f2b8eac6cdcfb3851f89
bluteaur <bluteaur@uwindsor.ca>
2015-07-26 13:28:51 -0400

    Update chat.php

--- chat.php
+++ chat.php
@@ -17,6 +17,52 @@ if($_SESSION['login'] == false || !isset($_SESSION['login']) || $_SESSION['bot']
     ?>
     <meta name="Cupcake Messaging" content="Your messaging site!">
     <meta name="Cupcake Messaging" content="We don't sell your information. You have control.">
+  <style>
+  <?php
+     
+     echo '#innerChat' . $_POST['id'] . " {\n";
+echo <<<EOF
+   text-color: #919191;
+   font-size:6vw;
+   text-align:left;
+}
+EOF;
+     echo '#innerChat' . $_SESSION['id'] . " {\n";
+echo <<<EOF
+   text-color: #000000;
+   font-size:6vw;
+   text-align:right;
+}
+EOF;
+  ?>
+  </style>
+  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script>
+    var from = <?php echo $_SESSION['id']; ?>;
+    $("form").on("submit", function (e) {
+        e.preventDefault();
+        var message = $("#message").val();
+        var id = $("#to").val();
+        if(message == '' || id == '' || id=='-1') {
+          return;
+        }
+        else {
+          var dataString = 'message='+ message + '& id=' + id;
+          var addMessage = '<div id = innerchat' + from + '>' + message + '</div>';
+          $.ajax({
+               type: "POST",
+               url: "MessageCenter.php",
+               data: dataString,
+               success: function(){
+                 var keep =  document.getElementById('chatbox').innerHTML;
+                 document.getElementById('chatbox').innerHTML = keep + addMessage;
+                 document.getElementById('message').innerHTML = "";
+               }
+           });
+        }
+    }
+  </script>
   </head>
   <body>
     <div id="LoginForm">
@@ -24,6 +70,15 @@ if($_SESSION['login'] == false || !isset($_SESSION['login']) || $_SESSION['bot']
        <?php
          if($_SESSION['mobile']){
             echo '<div id=chatbox><div id=innerChat'. $_POST['id'] .'>NoChatYet</div></div>';
+            echo <<<EOF
+            <form>
+              <input type="submit" id="send" name="send">
+EOF;
+echo '              <input type="hidden" name="to" value="'. $_POST['id'] .'">\n';
+echo <<<EOF
+              <textarea name="message" placeholder="Type something"></textarea>
+            </form>
+EOF;
          }
        ?>
        Received id: <?php echo $_POST['id']; ?><br />
